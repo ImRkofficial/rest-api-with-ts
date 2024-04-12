d979a596051b8b8fc2c6216d31d72eb1b5e9b875
imRkofficial <rkbagra0490@gmail.com>
2024-04-12 11:27:55 +0530

    add user model

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./users/usersRoutes";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res, next) => {
   res.json({ message: "Hello world" });
--- src/users/usersController.ts
+++ src/users/usersController.ts
@@ -1,5 +1,13 @@
 import { Request, Response, NextFunction } from "express";
+import createHttpError from "http-errors";
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { name, email, password } = req.body;
+
+  // Validation
+  if (!email || !name || !password) {
+    const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
   res.json({
     message: "User created",
   });
